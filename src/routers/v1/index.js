b1f29f932a954e04ef538a457feb96a260ebaaf9
Abhishek Das <abhishek.das1@lloydsbanking.com>
2019-03-23 12:27:53 +0000

    feat(errorHandler): error managment and user guild to valid URI

--- src/routers/v1/index.js
+++ src/routers/v1/index.js
@@ -1,8 +1,14 @@
 import { Router } from 'express';
 import documentRoute from './documentation';
+import errorHandler from '../../middleware/errorHandler';
 
 const v1RouteEntry = Router();
+const validRoutes = {
+  docsURI: '/docs',
+};
 
-v1RouteEntry.use('/docs', documentRoute);
+v1RouteEntry.use(validRoutes.docsURI, documentRoute);
+
+v1RouteEntry.use(errorHandler(validRoutes));
 
 export default v1RouteEntry;
